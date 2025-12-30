664cff811e4dc94733adb31f7647c5f0be34f273
p3rciv3l <avikrish001@gmail.com>
2025-12-30 17:24:46 -0500

    made generic

--- character/constants.py
+++ character/constants.py
@@ -1,12 +1,3 @@
-"""
-Constants file for character elicitation project.
-Set these paths to point to your local data and model directories.
-"""
-
 # Path to training and evaluation data
 # This is where preference elicitation results, robustness evaluations, etc. are stored
-DATA_PATH = "../data"
-
-# Path to local model files
-# This is where your downloaded models (e.g., qwen-2.5-7b-it, llama-3.1-8b-it) are stored
-MODEL_PATH = "./model"
+DATA_PATH = "/path/to/training_and_eval_data"
